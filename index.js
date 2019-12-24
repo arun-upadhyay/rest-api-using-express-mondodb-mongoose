b9cb63315797f443b575f57fe533fe63e088f029
Arun Upadhyay <arunnnet@gmail.com>
2019-12-24 12:08:33 -0500

    make it better

--- index.js
+++ index.js
@@ -115,5 +115,5 @@ app.delete('/posts', function (req, res) {
         })
     })
 })
-const port = 3000;
+const port = process.env.PORT ||  3000;
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))

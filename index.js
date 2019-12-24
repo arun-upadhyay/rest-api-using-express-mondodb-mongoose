8350aadc8d4fdcc08e5a85e403470c499182bc5d
Arun Upadhyay <arunnnet@gmail.com>
2019-12-24 14:31:32 -0500

    Reverting change again

--- index.js
+++ index.js
@@ -1,130 +1,124 @@
+// express
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(cors());
+app.use(morgan("tiny"));
 
-// Preamble
-var http = require ('http');	     // For serving a basic web page.
-var mongoose = require ("mongoose"); // The reason for this demo.
+//*** Database setup ****
+const mongoose = require('mongoose');
 
-// Here we find an appropriate database to connect to, defaulting to
-// localhost if we don't find one.
 var uristring =
     process.env.MONGODB_URI ||
-    'mongodb://localhost/HelloMongoose';
+    'mongodb://localhost/post_collections';
 
-// The http server will listen to an appropriate port, or default to
-// port 5000.
-var theport = process.env.PORT || 5000;
+//Set up default mongoose connection
+var connection = mongoose.createConnection(uristring,
+    {useNewUrlParser: true, useUnifiedTopology: true})
 
-// Makes connection asynchronously.  Mongoose will queue up database
-// operations and release them when the connection is complete.
-mongoose.connect(uristring, function (err, res) {
-    if (err) {
-        console.log ('ERROR connecting to: ' + uristring + '. ' + err);
-    } else {
-        console.log ('Succeeded connected to: ' + uristring);
-    }
-});
+const Posts = connection.model('Post', new mongoose.Schema({
+        title: {
+            type: String
+        },
+        description: {
+            type: String
+        }
+    }, {collection: 'Post'})
+);
+// **** End of database setup ****
 
-// This is the schema.  Note the types, validation and trim
-// statements.  They enforce useful constraints on the data.
-var userSchema = new mongoose.Schema({
-    name: {
-        first: String,
-        last: { type: String, trim: true }
-    },
-    age: { type: Number, min: 0}
-});
+app.get('/', (req, res) => res.send(`Supported endpoint = /posts, /posts/:id', methods = GET, PUT, DELETE, POST`))
 
-// Compiles the schema into a model, opening (or creating, if
-// nonexistent) the 'PowerUsers' collection in the MongoDB database
-var PUser = mongoose.model('PowerUsers', userSchema);
+// get all posts
+app.get('/posts', (req, res) => {
+    Posts.find()
+        .then(records => {
+            res.send(records)
+        }).catch(err => {
+        res.status(500).send({
+            message: `${err.message} occurred while fetching records`
+        })
+    })
 
-// Clear out old data
-PUser.remove({}, function(err) {
-    if (err) {
-        console.log ('error deleting old data.');
-    }
-});
-
-// Creating one user.
-var johndoe = new PUser ({
-    name: { first: 'John', last: 'Doe' },
-    age: 25
-});
-
-// Saving it to the database.
-johndoe.save(function (err) {if (err) console.log ('Error on save!')});
-
-// Creating more users manually
-var janedoe = new PUser ({
-    name: { first: 'Jane', last: 'Doe' },
-    age: 65
-});
-janedoe.save(function (err) {if (err) console.log ('Error on save!')});
-
-// Creating more users manually
-var alicesmith = new PUser ({
-    name: { first: 'Alice', last: 'Smith' },
-    age: 45
-});
-alicesmith.save(function (err) {if (err) console.log ('Error on save!')});
+})
 
 
-// In case the browser connects before the database is connected, the
-// user will see this message.
-var found = ['DB Connection not yet established.  Try again later.  Check the console output for error messages if this persists.'];
+app.get('/posts/:_id', (req, res) => {
+    Posts.findById(req.params._id)
+        .then(record => {
+            if (!record) {
+                return res.status(404).send({
+                    message: "Record not found. Invalid _id = ".req.params._id
+                })
+            }
+            return res.send(record);
+        }).catch(err => {
+        return res.status(500).send({
+            message: `${err.message} occurred while fetching record with _id ` + req.params._id
+        })
+    })
+})
 
-// Create a rudimentary http server.  (Note, a real web application
-// would use a complete web framework and router like express.js).
-// This is effectively the main interaction loop for the application.
-// As new http requests arrive, the callback function gets invoked.
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    createWebpage(req, res);
-}).listen(theport);
+// saving data
+app.post('/posts', function (req, res) {
+    const post = new Posts({
+        'title': req.body.title,
+        'description': req.body.description
+    })
+    post.save()
+        .then(record => {
+            res.send({
+                message: "Data saved",
+                record: record
+            })
+        }).catch(err => {
+        res.send({
+            message: `${err.message} occurred while saving data`
+        })
+    })
 
-function createWebpage (req, res) {
-    // Let's find all the documents
-    PUser.find({}).exec(function(err, result) {
-        if (!err) {
-            res.write(html1 + JSON.stringify(result, undefined, 2) +  html2 + result.length + html3);
-            // Let's see if there are any senior citizens (older than 64) with the last name Doe using the query constructor
-            var query = PUser.find({'name.last': 'Doe'}); // (ok in this example, it's all entries)
-            query.where('age').gt(64);
-            query.exec(function(err, result) {
-                if (!err) {
-                    res.end(html4 + JSON.stringify(result, undefined, 2) + html5 + result.length + html6);
-                } else {
-                    res.end('Error in second query. ' + err)
-                }
-            });
-        } else {
-            res.end('Error in first query. ' + err)
-        };
-    });
-}
+})
 
-// Tell the console we're getting ready.
-// The listener in http.createServer should still be active after these messages are emitted.
-console.log('http server will be listening on port %d', theport);
-console.log('CTRL+C to exit');
+// update record by Id
+app.put('/posts', function (req, res) {
+    let _id = req.body._id;
+    Posts.findByIdAndUpdate(_id, {
+        title: req.body.title,
+        description: req.body.description
+    }, {useFindAndModify: false})
+        .then(record => {
+            if (!record) {
+                return res.status(404).send({message: `Record not found with _id = ${_id}`})
+            }
+            return res.send({message: `Record updated for _id = ${_id}`})
+        })
+        .catch(err => {
+            return res.status(500).send({message: `${err.message} occurred while updating record with _id = ${_id}`})
+        })
+})
 
-//
-// House keeping.
-
-//
-// The rudimentary HTML content in three pieces.
-var html1 = '<title> hello-mongoose: MongoLab MongoDB Mongoose Node.js Demo on Heroku </title> \
-<head> \
-<style> body {color: #394a5f; font-family: sans-serif} </style> \
-</head> \
-<body> \
-<h1> hello-mongoose: MongoLab MongoDB Mongoose Node.js Demo on Heroku </h1> \
-See the <a href="https://devcenter.heroku.com/articles/nodejs-mongoose">supporting article on the Dev Center</a> to learn more about data modeling with Mongoose. \
-<br\> \
-<br\> \
-<br\> <h2> All Documents in MonogoDB database </h2> <pre><code> ';
-var html2 = '</code></pre> <br\> <i>';
-var html3 = ' documents. </i> <br\> <br\>';
-var html4 = '<h2> Queried (name.last = "Doe", age >64) Documents in MonogoDB database </h2> <pre><code> ';
-var html5 = '</code></pre> <br\> <i>';
-var html6 = ' documents. </i> <br\> <br\> \
-<br\> <br\> <center><i> Demo code available at <a href="http://github.com/mongolab/hello-mongoose">github.com</a> </i></center>';
+app.delete('/posts', function (req, res) {
+    let _id = req.body._id;
+    Posts.findByIdAndRemove(_id, {useFindAndModify: false})
+        .then(record => {
+            if (!record) {
+                return res.status(404).send({
+                    message: `Record not found with _id = ${_id}`
+                })
+            }
+            return res.send({
+                _id: `${_id}`,
+                message: `Record deleted with _id = ${_id}`
+            })
+        }).catch((err) => {
+        return res.status(500).send({
+            message: `${err.message} occurred while deleting record with _id = ${_id}`
+        })
+    })
+})
+const port = process.env.PORT ||  3000;
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))

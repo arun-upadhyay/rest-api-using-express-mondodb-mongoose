ee508ba9632aa1305bd8be90191dfe86f9642f89
Arun Upadhyay <arunnnet@gmail.com>
2020-03-08 12:02:34 -0400

    Added link of mongodb

--- conn/connection.js
+++ conn/connection.js
@@ -1,3 +1,3 @@
-const connection = require('mongoose').createConnection('mongodb://127.0.0.1/heroku_cddmrvc8',
+const connection = require('mongoose').createConnection('mongodb://heroku_cddmrvc8:vu88vm1mj46pl18m2m1gn4knsv@ds249787.mlab.com:49787/heroku_cddmrvc8',
     {useNewUrlParser: true, useUnifiedTopology: true});
 module.exports = connection;

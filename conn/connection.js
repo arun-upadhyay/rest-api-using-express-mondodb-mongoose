c4f4d9252cbb51410fbea4dded17e39af8747003
Arun Upadhyay <arunnnet@gmail.com>
2020-03-08 12:08:43 -0400

    changed URI

--- conn/connection.js
+++ conn/connection.js
@@ -1,3 +1,9 @@
-const connection = require('mongoose').createConnection('mongodb://heroku_cddmrvc8:vu88vm1mj46pl18m2m1gn4knsv@ds249787.mlab.com:49787/heroku_cddmrvc8',
+const URI = "mongodb://heroku_cddmrvc8:vu88vm1mj46pl18m2m1gn4knsv@ds249787.mlab.com:49787/heroku_cddmrvc8";
+/**
+ * For local machine
+ * const URI =  "mongodb://127.0.0.1/api_service"
+ */
+
+const connection = require('mongoose').createConnection(URI,
     {useNewUrlParser: true, useUnifiedTopology: true});
 module.exports = connection;

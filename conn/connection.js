d86805096aaca97dfa673583e1b73003945272b7
Arun Upadhyay <arunnnet@gmail.com>
2020-03-08 11:57:13 -0400

    Adding mongo link

--- conn/connection.js
+++ conn/connection.js
@@ -1,3 +1,3 @@
-const connection = require('mongoose').createConnection('mongodb://127.0.0.1/api_service',
+const connection = require('mongoose').createConnection('mongodb://127.0.0.1/heroku_cddmrvc8',
     {useNewUrlParser: true, useUnifiedTopology: true});
 module.exports = connection;
